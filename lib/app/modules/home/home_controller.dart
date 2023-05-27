a33072a6ef6e7569c6081eeb1dcbd209b92cb28a
Vishal96P <vishaloffical96@gmail.com>
2023-05-27 20:09:10 +0530

    Added Webview

--- lib/app/modules/home/home_controller.dart
+++ lib/app/modules/home/home_controller.dart
@@ -4,31 +4,11 @@ import 'package:get/get.dart';
 
 class HomeController extends GetxController {
   //TODO: Implement HomeController
-  //late final WebViewController webViewController;
-  RxBool isShowLoader = false.obs;
-  RxDouble progressValue = 0.0.obs;
   late InAppWebViewController webViewController;
 
   @override
   void onInit() {
     // TODO: implement onInit
     super.onInit();
-    /*webViewController = WebViewController()
-      ..setJavaScriptMode(JavaScriptMode.unrestricted)
-      ..setBackgroundColor(Get.theme.colorScheme.onPrimary)
-      ..setNavigationDelegate(
-        NavigationDelegate(
-          onProgress: (int progress) {
-          },
-          onPageStarted: (String url) {
-            EasyLoaderAnimation.showEasyLoader();
-          },
-          onPageFinished: (String url) {
-            EasyLoading.dismiss();
-          },
-          onWebResourceError: (WebResourceError error) {},
-        ),
-      )
-      ..loadRequest(Uri.parse('https://staging1.socialworkportal.com'));*/
   }
 }
